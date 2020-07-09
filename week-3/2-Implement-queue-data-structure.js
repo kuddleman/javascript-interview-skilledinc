c53fbf749d9e84be7d39eaab139d3b95bd47accc
Kuddleman <edl1155@gmail.com>
2020-07-09 16:38:56 -0700

    complete ex 2 week3

--- week-3/2-Implement-queue-data-structure.js
+++ week-3/2-Implement-queue-data-structure.js
@@ -2,18 +2,58 @@
 // with the standard enqueue and dequeue methods.
 // What is the complexity of each method?
 
+class Node {
+  constructor( value ) {
+    this.value = value
+    this.next = null
+  }
+}
+
 class MyQueue {
   
   constructor() {
-    // your code here
+    this.first = null
+    this.last = null
+    this.size = 0
   }
 
   enqueue(item) {
-   // your code here 
+   // create a new node
+   const newNode = new Node(item)
+   // if there is nothing in the queue, set first and last to newNode
+   if( !this.first ) {
+     this.first = newNode
+     this.last = newNode
+   } else {
+   // else set the next property on the current last to be newNode
+   // set the last property to be newNode
+     this.last.next = newNode
+     this.last = newNode
+   }
+     return ++this.size
   }
 
   dequeue() {
-    // your code here
+    // if there is no first property, return null
+    if ( !this.first ) {
+      return null
+    }
+    
+    let deQueuedFirst = this.first
+    //store the first property in a variable
+
+    //If the first same as the last( e.g only one node exists in the queue )
+    if ( this.first === this.last ){
+    //Set first and last to be null
+      this.first = null
+      this.last = null
+    } 
+    //else, set the first property to be the next property of first
+    this.first = this.first.next
+    //decrement size by one
+    this.size --
+    //return the value of the node dequeued
+    return deQueuedFirst
   }
   
 }
