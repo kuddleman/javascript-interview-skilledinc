a0bda7a79101ae4ce029a1c143a2d86874e40560
Kuddleman <edl1155@gmail.com>
2020-07-09 16:07:33 -0700

    complete ex1 week3

--- week-3/1-DONE-Rotate-a-2D-matrix.js
+++ week-3/1-DONE-Rotate-a-2D-matrix.js
@@ -4,6 +4,11 @@
 
 //https://code.likeagirl.io/rotate-an-2d-matrix-90-degree-clockwise-without-create-another-array-49209ea8b6e6
 
+//https://www.youtube.com/watch?v=wJJp6QLLXYE
+
+
+/// THIS ONE ******   https://www.youtube.com/watch?v=55_Sgk_sTrk
+
 let matrix =   [[1, 2, 3, 4],
                 [5, 6, 7, 8],
                 [9, 10, 11, 12],
@@ -12,7 +17,16 @@ let matrix =   [[1, 2, 3, 4],
 function rotate(matrix) {
     const newMatrix = [];
     
-    // your code here
+    const matrixLength = matrix.length
+    for( let i = 0; i < matrix.length; i++ ) {
+      newMatrix.push( [] )
+    }
+
+    for( let row = 0; row < matrixLength; row++ ) {
+      for( let elem = 0; elem < matrixLength; elem++ ) {
+        newMatrix[row].push( matrix[elem].pop() )
+      }
+    }
 
     return newMatrix;
 }
