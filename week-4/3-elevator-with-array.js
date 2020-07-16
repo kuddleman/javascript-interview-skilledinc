9d1ee1d2154e40a6c71da20c5ed44f7ee7825f19
Kuddleman <edl1155@gmail.com>
2020-07-15 19:19:15 -0700

    add comments

--- week-4/3-elevator-with-array.js
+++ week-4/3-elevator-with-array.js
@@ -55,29 +55,36 @@ console.log (allElevators[0].direction)
 // which elevator should be dispatched to the requested floor  call moveToFloor method 
 // on the appropriate elevator and return its id.
 
+// Dispatch elevators according to the following priorities:
+  // 1. Closest elevator AND moving towards the requested floor.
+  // 2. Closest elevator  AND that’s idle.
+  // 3. Closest elevator AND moving away from the requested floor.
+
 const dispatch = ( floor, direction ) => {
-  let numFloorsAway = 100
+  let leastNumFloorsAway = 200
   let indexOfClosest = 0
 
-  // then just find closest elevator
+  // then just find closest elevator to the calling floor( parameter: floor )
 
   for ( let i = 0; i < allElevators.length; i++ ) {
-     if ( Math.abs(allElevators[i].currentFloor - floor) < numFloorsAway ) {
-       numFloorsAway = Math.abs(allElevators[i].currentFloor - floor)
+     if ( Math.abs(allElevators[i].currentFloor - floor) < leastNumFloorsAway ) {
+       leastNumFloorsAway = Math.abs(allElevators[i].currentFloor - floor)
        indexOfClosest = i
      }
   }
-  // do we need to change the direction of the called elevator?
+  // do we need to change the direction of the called elevator 
+    // while it's being dispatched to the floor where it's needed?
+    // Here is some optional code to take care of that condition:
+  /*
   if ( allElevators[indexOfClosest].floor < floor ) {
     allElevators[indexOfClosest].direction = "up"
   } else if ( allElevators[indexOfClosest].floor > floor ) {
     allElevators[indexOfClosest].direction = "down"
   }
+  */
+
   allElevators[ indexOfClosest ].moveToFloor(floor)
+
+  return allElevators[ indexOfClosest ].id
 }
 
-// Dispatch elevators according to the following priorities:
-
-// 1. Closest elevator AND moving towards the requested floor.
-// 2. Closest elevator  AND that’s idle.
-// 3. Closest elevator AND moving away from the requested floor.
